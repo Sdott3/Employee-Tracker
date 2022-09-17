0805ae4d590eb0bef40e86f5392a1848bd1da37f
sdott3 <sdotthomas347@gmail.com>
2022-09-17 17:00:30 -0400

    created role table added to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,14 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
+
 USE employees_db;
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(25) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
 
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
