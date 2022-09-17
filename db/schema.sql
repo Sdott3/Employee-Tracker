b88bd9787ce983db4542d067dbbd2e96c3c25d69
sdott3 <sdotthomas347@gmail.com>
2022-09-17 18:36:47 -0400

    added role table to mysql and to db and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,12 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
--- CREATE TABLE role (
---   id INT AUTO_INCREMENT PRIMARY KEY,
---   title VARCHAR(25) UNIQUE NOT NULL,
---   salary DECIMAL NOT NULL,
---   department_id INT NOT NULL
--- );
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(25) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
 
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
