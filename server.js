a6af177325c1013c41c067fff0ceed5bc0b6dfee
sdott3 <sdotthomas347@gmail.com>
2022-09-18 05:46:31 -0400

    added & completed department.js

--- server.js
+++ server.js
@@ -10,55 +10,25 @@ db.connect(err => {
 });
 
 // prompt for user choices
-const userPrompt = () => {
-    return inquirer
-    .prompt([
-       {
-        type: "list",
-        message: "What would you like to do?",
-        name: "action",
-        choices: [
-            "View All Employees",
-            "View All Departments",
-            "View All Roles",
-            "Add Employee",
-            "Add Department",
-            "Add Role",
-            "Update Employee Role",
-            "Delete Role",
-            "Exit"
-            ]
-        } 
-    ])
+function userPrompt() {
+    inquirer.prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "action",
+                choices: [
+                    "View All Employees",
+                    "View All Departments",
+                    "View All Roles",
+                    "Add Employee",
+                    "Add Department",
+                    "Add Role",
+                    "Update Employee Role",
+                    "Delete Role",
+                    "Exit"
+                ]
+            }
+        ])
 
-    .then((selection) => {
-        const { menuSelect } = selection;
-        if (menuSelect === "View All Employees") {
-          viewEmployees();
-        }
-        if (menuSelect === "View All Departments") {
-          viewDepartments();
-        }
-        if (menuSelect === "View All Roles") {
-            viewRoles();
-        }
-        if (menuSelect === "Add Employee") {
-            addEmployee();
-        }
-        if (menuSelect === "Add Department") {
-            addDepartment();
-        }
-        if (menuSelect === "Add Role") {
-            addRole();
-        }
-        if (menuSelect === "Update Employee Role") {
-            updateRole();
-        }
-        if (menuSelect === "Delete Role") {
-            deleteRole();
-        }
-        if (menuSelect === "Exit") {
-            console.log("Thank You. Start the app over to use again.");
-        }
-    });
-};
+        
+}
